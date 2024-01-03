f0b186ca79dc7c272350ab4e27b3305dcfdd94f3
ali <a.aloreabi2000@gmail.com>
2024-01-03 18:24:16 +0300

    README.file edited

---
+++ src/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contacts">Contacts</a></li>
+            </ul>
+            <a href="https://github.com/alialoraebi">
+                <img src="https://github.com/alialoraebi.png" alt="GitHub Profile Picture" />
+            </a>
+        </nav>
+    );
+};
+
+export default Nav;
