a076e49b2357dc12357c43f4fd432d73f4afddbf
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-02-27 22:49:10 -0500

    changed projects screen

--- src/components/projects.js
+++ src/components/projects.js
@@ -21,21 +21,21 @@ const Projects = () => {
     ];
 
     return (
-        <section id="projects" className="bg-gray-100 py-8">
-            <div className="container mx-auto px-6 ">
-                <div className="space-y-6 ">
-                    {projects.map((project, index) => (
-                        <div key={index} className="bg-black shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300 ease-in-out">
-                            <h3 className="text-2xl mb-2 font-bold">{project.name}</h3>
-                            <p className="mb-4">{project.description}</p>
-                            <a href={project.repoUrl} className="text-blue-600 hover:text-blue-800 transition-colors duration-300 ease-in-out">
-                                <i className="fa fa-github" aria-hidden="true"></i> View on GitHub
-                            </a>
-                        </div>
-                    ))}
+        <section id="projects" className="bg-black-100 py-8">
+    <div className="container mx-auto px-6 ">
+        <div className="space-y-6 ">
+            {projects.map((project, index) => (
+                <div key={index} className="bg-black p-6 hover:shadow-xl transition-shadow duration-300 ease-in-out border-b border-dashed border-gray-500">
+                    <h3 className="text-2xl mb-2 font-bold">{project.name}</h3>
+                    <p className="mb-4">{project.description}</p>
+                    <a href={project.repoUrl} className="text-blue-600 hover:text-blue-800 transition-colors duration-300 ease-in-out">
+                        <i className="fa fa-github" aria-hidden="true"></i> View on GitHub
+                    </a>
                 </div>
-            </div>
-        </section>
+            ))}
+        </div>
+    </div>
+</section>
     );
 };
 
