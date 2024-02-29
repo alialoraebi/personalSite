ad0ce109cbd8a29ad301ebd719bee3cc56a0b3ee
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-02-29 01:30:22 -0500

    changed link target

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -8,15 +8,15 @@ const Header = () => {
                 <img src="https://github.com/alialoraebi.png" alt="GitHub Profile Picture" className="w-16 h-16 rounded-full" />
             </div>
             <div className="text-center xs:text-sm">
-                <h1 className="text-2xl xs:text-xl font-bold">Ali Al Aoraebi</h1>
+                <h1 className="text-2xl xs:text-xl font-bold">Ali Al Oraibi</h1>
                 <h2 className="text-xl xs:text-lg">Software Developer</h2>
                 <p className="xs:text-sm">Oakville, Ontario</p>
             </div>
             <div className="flex space-x-4">
-                <a href="https://github.com/alialoraebi" className="text-gray-400 hover:text-white">
+                <a href="https://github.com/alialoraebi" className="text-gray-400 hover:text-white" target='_blank' rel='noopener noreferrer'>
                     <FaGithub size={32} />
                 </a>
-                <a href="https://www.linkedin.com/in/ali-al-oraibi/" className="text-gray-400 hover:text-white">
+                <a href="https://www.linkedin.com/in/ali-al-oraibi/" className="text-gray-400 hover:text-white" target='_blank' rel='noopener noreferrer'>
                     <FaLinkedin size={32} />
                 </a>
             </div>
