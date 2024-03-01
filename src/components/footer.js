c330f61aae9e9260baf6fdd606ce2c771bde5577
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-02-29 20:00:25 -0500

    edited footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
       <footer className=" font-custom p-4 text-left">
-        © 2024 Ali Al Aoraebi
+        © 2024 Ali Al Oraibi
       </footer>
     );
 };
