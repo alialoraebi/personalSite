ee0d25ec67c4f8dbe3ccf550ad6dd3f141fb0059
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-03-01 23:52:35 -0500

    updated README.md file and made small UI changes on video

--- src/components/video.js
+++ src/components/video.js
@@ -7,7 +7,7 @@ const handleLinkClick = (e) => {
 const Video = ({ video }) => {
   return (
     <div key={video.id} onClick={handleLinkClick} className="p-4 bg-gray-800 rounded shadow-lg text-white">
-      <h3 className="text-sm font-bold mb-2">{video.title}</h3>  
+      <h3 className="text-sm xs:text-sm font-bold mb-2">{video.title}</h3>  
       <img src={video.icon} alt={video.title} className=" object-cover mb-2 rounded" />
       <a href={video.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 underline">
         Play Video
