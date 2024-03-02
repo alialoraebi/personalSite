4d0e68702d45410086fd8718edc13749df390c8e
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-03-01 23:58:31 -0500

    edited video section

--- src/components/video.js
+++ src/components/video.js
@@ -6,10 +6,10 @@ const handleLinkClick = (e) => {
 
 const Video = ({ video }) => {
   return (
-    <div key={video.id} onClick={handleLinkClick} className="p-4 bg-gray-800 rounded shadow-lg text-white">
+    <div key={video.id} onClick={handleLinkClick} className="p-4 bg-white/5 rounded-2xl shadow-lg text-white">
       <h3 className="text-sm xs:text-sm font-bold mb-2">{video.title}</h3>  
       <img src={video.icon} alt={video.title} className=" object-cover mb-2 rounded" />
-      <a href={video.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-700 underline">
+      <a href={video.link} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:text-blue-800 transition-colors duration-300 ease-in-out">
         Play Video
       </a>
     </div>
