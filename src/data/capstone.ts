e597518c6a154b9a99952557e6d667e8a3a6535b
Ali Al Oreabi <a.aloreabi2000@gmail.com>
2024-03-16 17:57:31 -0400

    capstone section edited

--- src/data/capstone.ts
+++ src/data/capstone.ts
@@ -1,28 +1,30 @@
 export const capstones = [
     {
         name: 'Project Summary',
-        pdfLink: '/capstone/Project_Summary.pdf',
+        pdfLink: '/capstone/Project_Summary.pdf'
     },
     {
         name: 'Project Vision',
-        pdfLink: '/capstone/Project_Vision.pdf',
+        pdfLink: '/capstone/Project_Vision.pdf'
     },
     {
         name: 'Project Plan',
-        pdfLink: '/capstone/Project_Plan.pdf',
+        pdfLink: '/capstone/Project_Plan.pdf'
     },
     {
         name: 'Requirements Analysis and Design',
-        pdfLink: '/capstone/Requirements_Analysis_Design.pdf',
+        pdfLink: '/capstone/Requirements_Analysis_Design.pdf'
     },
     {
         name: 'Status Reports',
-        pdfLink: '/capstone/Project_Report.pdf',
+        pdfLink: '/capstone/Project_Report.pdf'
     },
     {
         name: 'Wireframes/Mock ups',
-        pdfLink: 'https://www.figma.com/file/5pzsjaK2SdRjwgXVgnBYnt/Alvaro-Aguirre-MEza\'s-team-library?type=design&node-id=0%3A1&mode=design&t=8bzqzq1b5tEALjy0-1',
+        pdfLink: 'https://www.figma.com/file/5pzsjaK2SdRjwgXVgnBYnt/Alvaro-Aguirre-MEza\'s-team-library?type=design&node-id=0%3A1&mode=design&t=8bzqzq1b5tEALjy0-1'
     },
-
-
+    {
+        name: 'System Implementation',
+        pdfLink: 'https://github.com/alialoraebi/capstone'
+    }
 ]
