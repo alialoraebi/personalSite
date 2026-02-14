98a0466bd4a12513a60d57fab7d1b48d23970348
ali <a.aloreabi2000@gmail.com>
2026-02-14 22:11:37 +0300

    changed about section and bg collor

--- src/components/about.js
+++ src/components/about.js
@@ -25,20 +25,20 @@ const About = () => {
       <TransitionGroup>
         {isOpen && (
           <CSSTransition timeout={400} classNames="my-node" unmountOnExit>
-            <section id="about" className='mt-3 mb-3 m-9' onClick={handleLinkClick}>
+          <section id="about" className='mt-3 mb-3 m-9' onClick={handleLinkClick}>
             <h2>
-              Hey there, I'm Ali, a passionate full-stack web developer with a keen focus on React.js and Node.js. I thrive on the thrill of exploring new technologies, constantly seeking to broaden my skill set and deepen my understanding of the digital landscape.
+              Hey, I'm Ali. I'm a software developer focused on modern web applications.
             </h2>
             <br />
             <h2>
-              As a developer, I embrace the philosophy of Bill Hicks: "We are the facilitators of our own creative evolution." This resonates deeply with me as I see my role not just as a coder, but as a catalyst for innovation and progress. I believe in the power of excellent communication, critical thinking, and continuous reflection to drive meaningful change in both my work and the world around me.
+              I build things, experiment, and continuously improve my skills through real projects.
             </h2>
             <br />
             <h2>
-              With a blend of past experiences, my current state of expertise, and ambitious future goals, I approach each project with enthusiasm and dedication. My motivation stems from a belief in my ability to make a difference, not just in my own career but in the broader tech community. Feel free to connect with me on <a href="https://www.linkedin.com/in/ali-al-oraibi/" className='text-blue-500 hover:text-blue-800 transition-colors duration-300 ease-in-out' onClick={handleLinkClick} target='_blank' rel='noopener noreferrer'>LinkedIn</a>.
+              You can connect with me on <a href="https://www.linkedin.com/in/ali-al-oraibi/" className='text-blue-500 hover:text-blue-800 transition-colors duration-300 ease-in-out' onClick={handleLinkClick} target='_blank' rel='noopener noreferrer'>LinkedIn</a>.
             </h2>
           </section>
-          </CSSTransition>
+        </CSSTransition>
         )}
       </TransitionGroup>
     </div>
